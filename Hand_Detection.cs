afd5f7e84597e958427a34cadc0e623c0881d308
Sensei-game <48223462+Sensei-game@users.noreply.github.com>
2021-01-04 10:34:04 +0200

    Hand_detection file for Unity in Assets folder
    This is the best version and the most stable one (simplified). 
    
    Needs testing with Oculus Quest 2 for confirmation and adjustments.

---
+++ Hand_Detection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public struct Gesture
+{
+    public string name;
+    public List<Vector3> Data_line;
+    public UnityEvent is_Recognized;
+}
+
+public class Hand_Detection : MonoBehaviour
+{
+
+    public float value = 0.1f;
+    public OVRSkeleton skeleton;
+    public List<Gesture> Gestures;
+    public bool allow_input = true;
+    private List<OVRBone> point_Bones;
+    private Gesture lastGesture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        point_Bones = new List<OVRBone>(skeleton.Bones);
+        lastGesture = new Gesture();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sanity check, and allow capture via SpaceBar
+        if(allow_input && Input.GetKeyDown(KeyCode.Space))
+        {
+            Save_Gesture();
+        }
+
+       // Gesture thisGesture = Recognize_Gesture();
+        //bool confirmation = !thisGesture.Equals(new Gesture());
+    }
+
+    //Capture Gestures Method
+   void Save_Gesture()
+    {
+        Gesture gesture = new Gesture();
+        gesture.name = "New Gesture";
+        List<Vector3> data = new List<Vector3>();
+
+        foreach(var point in point_Bones)
+        {
+            //semi-accurate position, relative to the root position
+            data.Add(skeleton.transform.InverseTransformPoint(point.Transform.position));
+        }
+        gesture.Data_line = data;
+        Gestures.Add(gesture);
+    }
+}
